a99f953cc8256ebc4038261870e71d7a2f359097
hi0yoo <yoo971202@naver.com>
2023-01-31 13:14:57 +0900

    init

---
+++ src/main/java/com/comeon/websocket/ComeOnAppWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.comeon.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeOnAppWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeOnAppWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comeon/websocket/ComeOnAppWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeon.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeOnAppWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
